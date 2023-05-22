54dc768ad3d26984b6b6bcbd2f9a5567983d5962
h756741 <115848218+h756741@users.noreply.github.com>
2023-05-22 13:14:53 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// backend requirements
+const express = require('express');
+const request = require('request-promise');
+
+// intialize app
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// keys
+const apiKey = '4a7d28e4857f4ff11ccbda830c765d6e';
+const baseUrl = `https://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+// parse json
+app.use(express.json());
+
+// initial route route
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API.');
+})
+
+// get product response
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+// get product reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+// get product offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+// get search results
+app.get('/search/:searchQuery', async (req, res) => {
+    const { searchQuery } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+// listen to port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
